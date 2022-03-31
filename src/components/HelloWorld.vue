8d691360d02d58df8ba8be0b7179b6d91e7a916e
SofiRD <a01657055@itesm.mx>
2022-03-30 22:01:44 -0600

    3 files modified, 2 added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,54 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref } from 'vue';
 defineProps({
   msg: String
-})
+});
 
-const count = ref(0)
+const count = ref(0);
+var existentialQuestion= ref('Que es tan lindo')
+var number = 0;
+var image_name = ref("../assets/michi1.jpg");
+var imagenes = [true, false, false, false];
+var mostrar = ref(true);
+var items= [
+      { message: 'Obediente' },
+      { message: 'Cariñoso' },
+      { message: 'Limpio' }
+    ]
+function changeImage() {  
+  for(var i = 0; i<imagenes.length; i++){
+     if (imagenes[i]){
+       if (i+1 < imagenes.length){
+         imagenes[i+1]=true
+       } else {
+         imagenes[0] = true
+       }
+       imagenes[i] = false;
+       break;
+     }
+    
+  }
+  items = imagenes
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <img class="image" v-show="mostrar" src="../assets/michi1.jpg" width="200" />
+  <img class="image" v-show="!mostrar" src="../assets/michi2.jpg" width="200" />
+  <h2>Lo que a tu te gusta de mi michi es: {{existentialQuestion}}</h2>
+   <input v-model="existentialQuestion"/>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+  <h2>¿Porque es el mejor? </h2>
+  <ul id="example-1">
+  <li v-for="item in items" :key="item.message">
+    {{ item.message }}
+  </li>
+</ul>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <button type="button" @click="mostrar = !mostrar">Cambiar Imagen</button>
+  
+  
 </template>
 
 <style scoped>
